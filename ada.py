c5cfdf77ab5a6ff417df32c66c980a7e3efb38f3
unnisama <79842734+unnisama@users.noreply.github.com>
2021-03-27 00:03:06 +0530

    MAke sense

--- ada.py
+++ ada.py
@@ -27,26 +27,26 @@ while True:
     for i in range(1,int(CLIP_FRAMES/30)):
         if(i <= CLIP_FRAMES):
             if(frames[((i-1)*30)+3] != frames[(i-1)*30]):
-                sundi("Bot1father", frames[((i-1)*30)+3], chat_id)
+                sundi("Bot1token", frames[((i-1)*30)+3], chat_id)
             if(frames[((i-1)*30)+3] != frames[((i-1)*30)+6]):
-                sundi("Bot2fAther", frames[((i-1)*30)+6], chat_id)
+                sundi("Bot2token", frames[((i-1)*30)+6], chat_id)
             if(frames[((i-1)*30)+6] != frames[((i-1)*30)+9]):
-                sundi("Bot3fAther", frames[((i-1)*30)+9], chat_id)
+                sundi("Bot3token", frames[((i-1)*30)+9], chat_id)
             if(frames[((i-1)*30)+9] != frames[((i-1)*30)+12]):
-                sundi("Bot4fAther", frames[((i-1)*30)+12], chat_id)
+                sundi("Bot4token", frames[((i-1)*30)+12], chat_id)
             if(frames[((i-1)*30)+12] != frames[((i-1)*30)+15]):
-                sundi("Bot5fAther", frames[((i-1)*30)+15], chat_id)
+                sundi("Bot5token", frames[((i-1)*30)+15], chat_id)
             if(frames[((i-1)*30)+15] != frames[((i-1)*30)+18]):
-                sundi("Bot6fAther", frames[((i-1)*30)+18], chat_id)
+                sundi("Bot6token", frames[((i-1)*30)+18], chat_id)
             if(frames[((i-1)*30)+18] != frames[((i-1)*30)+21]):
-                sundi("Bot7fAther", frames[((i-1)*30)+21], chat_id)
+                sundi("Bot7token", frames[((i-1)*30)+21], chat_id)
             if(frames[((i-1)*30)+21] != frames[((i-1)*30)+24]):
-                sundi("Bot8fAther", frames[((i-1)*30)+24], chat_id)
+                sundi("Bot8token", frames[((i-1)*30)+24], chat_id)
             if(frames[((i-1)*30)+24] != frames[((i-1)*30)+27]):
-                sundi("Bot9fAther", frames[((i-1)*30)+27], chat_id)
+                sundi("Bot9token", frames[((i-1)*30)+27], chat_id)
             if(frames[((i-1)*30)+27] != frames[((i-1)*30)+30]):
-                sundi("Bot10fAther", frames[((i-1)*30)+30], chat_id)
-    sundi("Bot10fAther", "As a noob trying to prevent from flood wait 20minutes", chat_id)
+                sundi("Bot10token", frames[((i-1)*30)+30], chat_id)
+    sundi("Bot10token", "As a noob trying to prevent from flood wait 20minutes", chat_id)
     if(os.environ.get('APP_URL') != None):
         for d in range(20):
             requests.get(os.environ["APP_URL"])
